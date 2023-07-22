63c3954ab6fac8a292c5d46746e70ca8573ec5f4
ayano-kobayashi-lvgs <ayano.kobayashi@leverages.jp>
2023-07-22 15:44:34 +0900

    component: our service

---
+++ todo/components/PageContents/Top/Services/Services.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import styles from './Services.module.scss';
+import Image from 'next/image';
+
+type ServiceProps = {
+  alt: string;
+  src: string;
+  description: string;
+  reflink: string;
+}
+
+const Services: React.FC = () => {
+  const serviceList: ServiceProps[] = [
+    {
+      alt: 'WeXpatsJobs',
+      src: '/img/common/logo/logo_WexpatsJobs.svg',
+      description: 'WeXpats Job is a website for job search for the non-Japanese people who are looking to work in Japan. WeXpats Job has one of the largest pools of jobs which welcome foreign employment. With WeXpats Job, you can search jobs in your own language!',
+      reflink: 'https://we-xpats.com/en/job/as/jp'
+    },
+    {
+      alt: 'WeXpatsSchool',
+      src: '/img/common/logo/logo_WeXpatsSchool.svg',
+      description: 'WeXpats School is an agent which specializes in service for studying abroad for the people who are thinking about studying in Japan. Our school provides a full range of services from consultation for study abroad to living in Japan.',
+      reflink: 'https://we-xpats.com/en/school/as/jp/'
+    },
+    {
+      alt: 'WeXpatsNihongo',
+      src: '/img/common/logo/logo_WeXpatsNihongo.svg',
+      description: 'WeXpats Nihongo is a language learning service for non-Japanese people. The method of active learning is used for practical vocal class.',
+      reflink: 'https://lp.we-xpats.com/vn/nihongo/vi/'
+    }
+  ]
+
+  return (
+    <div className={styles.contentBox}>
+      <h3 className={styles.title}>OUR SERVICE</h3>
+      <div className={styles.serviceList}>
+        {serviceList.map((service, index) => {
+          return (
+            <div key={index}>
+              <p className={styles.img}>
+                <Image src={service.src} alt={service.alt} height={56} width={300}></Image>
+              </p>
+            <div className={styles.descriptionArea}>
+              <p className={styles.description}>{service.description}</p>
+              <p className={styles.button}>
+                <a href={service.reflink} target="_blank">
+                  Read more
+                  <span className={styles.buttonIcon}></span>
+                </a>
+              </p>
+            </div>
+          </div>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Services;
--- todo/pages/index.tsx
+++ todo/pages/index.tsx
@@ -1,230 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Button } from "@components/Button";
-import { Input } from "@components/Input";
 import styles from "@styles/Home.module.scss"
 import Link from 'next/link'
+import Services from '../components/PageContents/Top/Services/Services';
 
 const Index: NextPage = () => {
-    // 型定義======
-    type Todo = {
-        id: number;
-        title: string;
-        editMode: boolean;
-        enabled: boolean;
-    }
-
-    // サンプル=======
-    const sampleTodoList = [
-        { id: 0, title: 'React参考書読む', editMode: false, enabled: true },
-        { id: 1, title: 'Laravel環境構築', editMode: false, enabled: true },
-        { id: 2, title: 'インフラ勉強', editMode: false, enabled: true },
-    ];
-
-    // 状態管理=======
-    const [todoList, setTodoList] = useState<Todo[]>([]);
-    const [inputText, setInputText] = useState('');
-    const [editText, setEditText] = useState('');
-    const [flashMessages, setFlashMessages] = useState({ todoTitle: '', message: '' });
-    const [isShowMessage, setIsShowMessage] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-
-    // 表示モード切り替え=======
-    const enableEditState = (id: number) => {
-        setTodoList(todoList.map((todo) => todo.id === id ? { id: todo.id, title: todo.title, editMode: true, enabled: todo.enabled } : todo));
-    }
-    const disableEditState = (id: number) => {
-        setTodoList(todoList.map((todo) => todo.id === id ? { id: todo.id, title: todo.title, editMode: false, enabled: todo.enabled } : todo));
-    }
-
-    // 追加・取消の表示切り替え========
-    const [buttonState, setButtonState] = useState('plus');
-    const enableMinusButton = () => {
-        setButtonState('minus');
-    }
-    const enablePlusButton = () => {
-        setButtonState('plus');
-    }
-
-    // 登録処理=======
-    const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputText(e.target.value);
-    }
-    // IDの自動連番
-    let nextId = todoList.length;
-
-    const addTodo = () => {
-        const newTodo: Todo = {
-            id: nextId,
-            title: inputText,
-            editMode: false,
-            enabled: true,
-        }
-        setTodoList([...todoList, newTodo]);
-        setInputText('');
-        nextId++;
-        console.log(todoList);
-    };
-
-    useEffect(() => {
-        console.log('test');
-    }, []);
-
-    //useEffect(() => {
-    //    setTimeout(() => {
-    //        setTodoList(sampleTodoList);
-    //        setIsLoading(false);
-    //    }, 1000);
-    //}, []);
-
-    //useEffect(() => {
-    //    if (todoList.length === 0) return;
-    //    console.log(todoList);
-    //    setIsShowMessage(true);
-    //    setTimeout(() => {
-    //        setIsShowMessage(false);
-    //    }, 500);
-    //}, [todoList]);
-
-    const handleChangeEdit = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEditText(e.target.value);
-    }
-
-    // 更新処理=======
-    const updateTodo = (id: number) => {
-        setTodoList((prevTodo) => prevTodo.map((todo) => todo.id === id ? { id: todo.id, title: editText, editMode: false, enabled: todo.enabled } : todo));
-        setFlashMessages({ todoTitle: editText, message: 'が更新されました！' });
-        setEditText('');
-    }
-
-    // 削除処理=======
-    const disableTodo = (id: number, title: string) => {
-        const todoTitle = title;
-        setTodoList(todoList.map((todo) => todo.id === id ? { id: todo.id, title: todo.title, editMode: todo.editMode, enabled: false } : todo));
-        setFlashMessages({ todoTitle: todoTitle, message: 'が削除されました！' });
-    };
-
-    return (
-        <div>
-            <Head>
-                <title>TODO</title>
-                <meta name="description" content="これはTODO作成アプリです。" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <header>
-                <p className={styles.title}>TODOリスト</p>
-            </header>
-            <main>
-                {buttonState === 'plus' ?
-                    <div>
-                        <Button
-                            onClick={enableMinusButton}
-                            className={styles.show}>追加
-                        </Button>
-                    </div>
-                    :
-                    <div>
-                        <Button
-                            onClick={enablePlusButton}
-                            className={styles.hide}>
-                            取消
-                        </Button>
-                    </div>
-                }
-                {isLoading && <p>Loading...</p>}
-                {isShowMessage && <p>登録されました！</p>}
-                {(buttonState === 'minus') &&
-                    <div>
-                        <form>
-                            <Input
-                                type="text"
-                                value={inputText}
-                                onChange={handleChangeInput}
-                                className={styles.input}
-                                required />
-                            <Button
-                                disabled={inputText === ""}
-                                onClick={() => addTodo()}
-                                className={styles.primary}>
-                                登録
-                            </Button>
-                        </form>
-                    </div>
-                }
-                <div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>タイトル</th>
-                                <th></th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {todoList.map((todo) => todo.enabled &&
-                                <tr key={todo.id}>
-                                    {(todo.editMode) ?
-                                        <>
-                                            <td colSpan={3}>
-                                                <form
-                                                    action="">
-                                                    <Input
-                                                        type="text"
-                                                        value={editText}
-                                                        onChange={handleChangeEdit}
-                                                        className={styles.input}
-                                                        required />
-                                                    <Button
-                                                        disabled={editText === ""}
-                                                        onClick={() => updateTodo(todo.id)}
-                                                        className={styles.primary}>
-                                                        更新
-                                                    </Button>
-                                                </form>
-                                                <Button
-                                                    onClick={() => disableEditState(todo.id)}
-                                                    className={styles.tertiary}>
-                                                    取り消し
-                                                </Button>
-                                            </td>
-                                        </>
-                                        :
-                                        <>
-                                            <td>{todo.title}</td>
-                                            <td>
-                                                <Button
-                                                    onClick={() => enableEditState(todo.id)}
-                                                    className={styles.primary}>
-                                                    編集
-                                                </Button>
-                                            </td>
-                                            <td>
-                                                <Button
-                                                    onClick={() => disableTodo(todo.id, todo.title)}
-                                                    className={styles.secondary}>
-                                                    削除
-                                                </Button>
-                                            </td>
-                                        </>
-                                    }
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
-                </div>
-            </main >
-            <footer>
-                <div>
-                    詳しくは
-                    <Link
-                        href="/todo/details"
-                        className={styles.link}>こちら</Link>
-                    をクリックしてください。
-                </div>
-            </footer>
-        </div >
-    );
-};
+  return (
+    <div>
+      <Head>
+        <title>Leverages Career Vietnam</title>
+        <meta name="description" content="A fast growing global recruitment agency pursuing the happiness of all people concerned. Create and leverage your future career with our experienced consultants." />
+        <link rel="icon" href="/img/common/icon/favicon.ico" />
+        <link rel="preconnect"href="https://fonts.gstatic.com" crossOrigin={"anonymous"} />
+        <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Roboto:wght@100;300;400;500;700;900&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" media="print" onLoad="this.media='all'" />
+      </Head>
+      <main>
+        <Services />
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
 
 export default Index;
